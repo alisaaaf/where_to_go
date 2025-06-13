4315e9381d1f12c4c02002a4cabd76200b9c0de7
alisaaaf <alicefwiu@gmail.com>
2025-06-13 15:28:39 +0200

    Move data from template to view

--- places/urls.py
+++ places/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from places import views
 
 urlpatterns = [
-    path('', views.show_phones),
+    path('', views.index),
 ]
--- places/views.py
+++ places/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import Place
 
 
-def show_phones(request):
-    return render(request, 'mainpage.html')
+def index(request):
+    places = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for place in Place.objects.all():
+        places["features"].append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lon, place.lat],
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": ""
+            }
+        })
+
+    return render(request, 'mainpage.html', {'geo_json': places})
